8357177d6d8d0a944c98d0b3df3da1871fbcf4e3
rmanaem <rmanaem@protonmail.ch>
2026-06-10 13:15:06 -0400

    chore: align eslint config with the one in annotation-tool

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,7 @@ import cypressPlugin from 'eslint-plugin-cypress';
 import reactPlugin from 'eslint-plugin-react';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import reactRefreshPlugin from 'eslint-plugin-react-refresh';
-import importPlugin from 'eslint-plugin-import';
-import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import tsdocPlugin from 'eslint-plugin-tsdoc';
-import eslintConfigPrettier from 'eslint-config-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -18,10 +14,6 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   cypressPlugin.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
-  jsxA11yPlugin.flatConfigs.recommended,
-  eslintPluginPrettierRecommended,
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
@@ -31,10 +23,12 @@ export default tseslint.config(
       tsdoc: tsdocPlugin,
     },
     languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
+      parserOptions: {
+        project: ['./tsconfig.*json'],
+      },
       globals: {
         ...globals.browser,
+        ...globals.es2020,
         ...globals.node,
       },
     },
@@ -44,30 +38,16 @@ export default tseslint.config(
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react/react-in-jsx-scope': 'off',
       'tsdoc/syntax': 'warn',
-      // Disabling this rule allows us to use blanket exports from components
-      'import/prefer-default-export': 'off',
       /* 
-      The fork of the eslint-config-airbnb-typescript package has added ESLint Stylistic plugin
-      to the config. see:https://github.com/Kenneth-Sills/eslint-config-airbnb-typescript/pull/3
-      Some of the stylistic rules are not compatible with our current prettier config so we disable them.
+      @typescript-eslint/no-unused-vars is turned off since eslint wasn't picking up on using `_` for unused variables
+      which is the solution that we've been using for it.
       */
-      '@stylistic/indent': 'off',
-      '@stylistic/comma-dangle': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
     settings: {
       react: {
         version: 'detect',
       },
-      'import/parsers': {
-        '@typescript-eslint/parser': ['.ts', '.tsx'],
-      },
-      'import/resolver': {
-        typescript: {
-          alwaysTryTypes: true,
-          project: ['./tsconfig.*json'],
-        },
-      },
     },
-  },
-  eslintConfigPrettier
+  }
 );
